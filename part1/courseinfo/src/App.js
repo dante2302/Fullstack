46e99278d3bd2a20e5e6497ff5243f2444e8f4e7
Dante <guchoo923@gmail.com>
2022-03-23 21:30:32 +0200

    Commit exercise 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -2,27 +2,27 @@ import { Header } from "tar";
 
 const App = () => {
   const course = "Half stack application development";
-  const part1 = {
+  const parts = [
+    {
     name :"Fundamentals of React",
-    exercises : 10}
-  const part2 = { 
+    exercises : 10
+    },
+    { 
     name: "Using props to pass data",
-    exercises = 7}
-  const part3 = {
+    exercises = 7
+    },
+    {
     name : "State of component",
     exercises : 14
-  }
+    }
+  ]
 
 return(
-//   <div>
-//   <><Header course={course} />
-//   <Content 
-//   Part1={part1.name part1.exercises} 
-//   Part2={part2.name ,part2.exercises}
-//   Part3={part3.name ,part3.exercises}
-//   />
-//   <Total Number of exercises {...part1.exercises + part2.exercises + part3.exercises} /></>
-//   </div>
-//   )
-// }  
+  <div>
+  <><Header course={course} />
+  <Content parts={parts}/>
+  <Total parts={parts} /></>
+  </div>
+  )
+}  
 export default app
